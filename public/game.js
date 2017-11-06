38670c8e65a122b7f935faccc88422cd219e3652
Matt Kalinowski <mkalinowski@pivotal.io>
2017-11-05 22:48:07 -0800

    init commit

---
+++ public/game.js
@@ -0,0 +1,75 @@
+'use strict';
+
+function PingPong() {
+    this.winCondition = 11;
+    this.winDelta = 2;
+
+    this.teams = [{
+        score: 0,
+        players: 0,
+        id: 'Team A',
+        serving: false
+    }, {
+        score: 0,
+        players: 0,
+        id: 'Team B',
+        serving: false
+    }];
+
+    this.totalScore = function() {
+        return  this.teams[0].score + this.teams[1].score;
+    };
+
+    this.diffScore = function() {
+        return Math.abs(this.teams[0].score - this.teams[1].score);
+    };
+
+    this.updateServing = function() {
+        const teamAServes = (this.totalScore() & 2) == 0;
+
+        this.teams[0].serving = teamAServes;
+        this.teams[1].serving = !teamAServes;
+    };
+
+    this.reset = function () {
+        Array.prototype.forEach.call(this.teams, function (t, idx) {
+            t.score = 0;
+            t.serving = false;
+        });
+
+        this.teams[0].serving = true;
+        this.updateServing();
+    };
+
+    this.scorePoint = function(teamIdx, numPoints) {
+        this.teams[teamIdx].score += numPoints;
+
+        if ((this.totalScore() >= this.winCondition) && (this.diffScore() >= this.winDelta))
+        {
+            this.reset();
+            return;
+        }
+
+        this.updateServing();
+    };
+
+    this.handleButtonPress = function(btn_id, duration) {
+        if (duration < 50) {
+            return;
+        }
+
+        if (btn_id == "reset") {
+            this.reset();
+            return;
+        }
+
+        let points = 1;
+        if (duration > 1000) {
+            points = -1;
+        }
+
+        this.scorePoint(btn_id == "a" ? 0 : 1, points);
+    };
+
+    this.reset();
+};
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.get('/', function(req, res){
+    res.render('/index.html');
+});
+app.get('/ping', function(req, res){
+    res.send('pong');
+});
+
+app.post('/', function (req, res) {
+    console.log(req.body);
+    io.emit('button press', req.body);
+    res.json(req.body);
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+http.listen(80, function(){
+    console.log('listening on *:80');
+});
