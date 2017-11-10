26ce208165739dfe87e4870cba297894b8594c48
Matt Kalinowski <mkalinowski@pivotal.io>
2017-11-09 19:43:07 -0800

    added some unit tests

--- spec/pingPongSpec.js
+++ spec/pingPongSpec.js
@@ -1,3 +1,12 @@
+function aServes(pp) {
+    expect(pp.teams[0].serving).toBe(true);
+    expect(pp.teams[1].serving).toBe(false);
+}
+function bServes(pp) {
+    expect(pp.teams[0].serving).toBe(false);
+    expect(pp.teams[1].serving).toBe(true);
+}
+
 describe("The PingPong app", function() {
     var PingPong = require('../public/game');
     it("should initialize to a blank game", function() {
@@ -10,5 +19,83 @@ describe("The PingPong app", function() {
         var pp = new PingPong();
         expect(pp.teams[0].serving).toBe(true);
         expect(pp.teams[1].serving).toBe(false);
+    });
+
+    it("should declare win condition points the winner", function() {
+        var pp = new PingPong();
+        pp.scorePoint(0, pp.winCondition);
+        expect(pp.teams[0].winner).toBe(true);
+        expect(pp.teams[1].winner).toBe(false);
+    });
+
+    it("should not declare win condition if not exceeding the delta", function() {
+        var pp = new PingPong();
+        pp.scorePoint(0, pp.winCondition - 1);
+        pp.scorePoint(1, pp.winCondition - 1);
+
+        expect(pp.teams[0].winner).toBe(false);
+        expect(pp.teams[1].winner).toBe(false);
+
+        pp.scorePoint(0, 1);
+
+        expect(pp.teams[0].winner).toBe(false);
+        expect(pp.teams[1].winner).toBe(false);
+
+        pp.scorePoint(0, 1);
+
+        expect(pp.teams[0].winner).toBe(true);
+        expect(pp.teams[1].winner).toBe(false);
+    });
+
+    it("should alternate who serves every 2 points", function() {
+        var pp = new PingPong();
+
+        aServes(pp);
+        pp.scorePoint(0, 1);
+        aServes(pp);
+        pp.scorePoint(0, 1);
+
+        bServes(pp);
+        pp.scorePoint(0, 1);
+        bServes(pp);
+        pp.scorePoint(0, 1);
+
+        aServes(pp);
+        pp.scorePoint(1, 1);
+        aServes(pp);
+        pp.scorePoint(1, 1);
+
+        bServes(pp);
+        pp.scorePoint(1, 1);
+        bServes(pp);
+        pp.scorePoint(1, 1);
+
+        aServes(pp);
+        pp.scorePoint(0, 1);
+        aServes(pp);
+        pp.scorePoint(1, 1);
+
+        bServes(pp);
+        pp.scorePoint(1, 1);
+        bServes(pp);
+        pp.scorePoint(0, 1);
+
+        aServes(pp);
+        pp.scorePoint(0, 1);
+        aServes(pp);
+        pp.scorePoint(0, 1);
+
+        bServes(pp);
+        pp.scorePoint(0, 1);
+        bServes(pp);
+        pp.scorePoint(0, 1);
+
+        aServes(pp);
+        pp.scorePoint(1, 1);
+        aServes(pp);
+        pp.scorePoint(0, 1);
+
+        expect(pp.teams[0].winner).toBe(true);
+        expect(pp.teams[1].winner).toBe(false);
     });
 });
